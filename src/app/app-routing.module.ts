2bdf74eccc8018afdd78d6b68e07991d5af261b8
Tobias Haller <tobias_haller@outlook.de>
2020-06-24 13:22:34 +0200

    Redone page not found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { AlgorithmViewComponent } from "./components/algorithms/algorithm-view/algorithm-view.component";
-import { SoftwarePlatformViewComponent } from "./components/software-platforms/software-platform-view/software-platform-view.component";
-import { CloudServiceViewComponent } from "./components/cloud-services/cloud-service-view/cloud-service-view.component";
-import { PublicationViewComponent } from "./components/publications/publication-view/publication-view.component";
+import { AlgorithmViewComponent } from './components/algorithms/algorithm-view/algorithm-view.component';
+import { SoftwarePlatformViewComponent } from './components/software-platforms/software-platform-view/software-platform-view.component';
+import { CloudServiceViewComponent } from './components/cloud-services/cloud-service-view/cloud-service-view.component';
+import { PublicationViewComponent } from './components/publications/publication-view/publication-view.component';
 
 const routes: Routes = [
   { path: 'algorithms', component: AlgorithmViewComponent },
