5634fd6b91bb499ae245637d5deda4f6d60619f4
Stefan Basaric <stefan.basaric@hotmail.de>
2020-06-24 13:49:28 +0200

    Fix more ESLint errors

--- src/app/components/cloud-services/cloud-service-view/cloud-service-view.component.ts
+++ src/app/components/cloud-services/cloud-service-view/cloud-service-view.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cloud-service-view',
   templateUrl: './cloud-service-view.component.html',
-  styleUrls: ['./cloud-service-view.component.scss']
+  styleUrls: ['./cloud-service-view.component.scss'],
 })
 export class CloudServiceViewComponent implements OnInit {
   constructor() {}
