d3dfe52c0fb1d97b51161d6e450505e32598c164
Stefan Basaric <stefan.basaric@hotmail.de>
2020-08-02 11:02:37 +0200

    Fixed ESlint errors

--- src/app/components/algorithms/dialogs/add-pattern-relation-dialog.component.ts
+++ src/app/components/algorithms/dialogs/add-pattern-relation-dialog.component.ts
@@ -99,28 +99,29 @@ export class AddPatternRelationDialogComponent implements OnInit {
     // If Input-Field not empty and input type does not exist
     if (!existingRelationType && this.patternRelationType.value) {
       // If pattern type does not exist and first element is existing type
-      if (this.patternRelationTypes.length < 1 || (this.patternRelationTypes[0] && this.patternRelationTypes[0].id)) {
+      if (
+        this.patternRelationTypes.length < 1 ||
+        (this.patternRelationTypes[0] && this.patternRelationTypes[0].id)
+      ) {
         // Push new type to array that needs to be created on the fly
-        this.patternRelationTypes.unshift({ name: this.patternRelationType.value });
+        this.patternRelationTypes.unshift({
+          name: this.patternRelationType.value,
+        });
         this.onPatternRelationTypeSelect(this.patternRelationTypes[0]);
-      }
-
-      else if (!this.patternRelationTypes[0].id) {
+      } else if (!this.patternRelationTypes[0].id) {
         this.patternRelationTypes[0] = { name: this.patternRelationType.value };
         this.onPatternRelationTypeSelect(this.patternRelationTypes[0]);
       }
-    }
-    // If Input-Field not empty and pattern exists
-     else if (existingRelationType && this.patternRelationType.value) {
+    } else if (existingRelationType && this.patternRelationType.value) {
       if (!this.patternRelationTypes[0].id) {
         this.patternRelationTypes.shift();
         this.onPatternRelationTypeSelect(existingRelationType);
       }
-    }
-
-    // If Input-Field is empty
-    else if (!this.patternRelationType.value) {
-      if (this.patternRelationTypes.length > 0 && !this.patternRelationTypes[0].id) {
+    } else if (!this.patternRelationType.value) {
+      if (
+        this.patternRelationTypes.length > 0 &&
+        !this.patternRelationTypes[0].id
+      ) {
         this.patternRelationTypes.shift();
         this.onPatternRelationTypeSelect(undefined);
       }
