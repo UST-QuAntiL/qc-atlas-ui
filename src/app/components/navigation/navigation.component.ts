c88d382aab54f6cf1ac1662ecae456051ec5a970
Valentin Seifermann <ValeSayfa@t-online.de>
2020-08-12 12:06:55 +0200

    changed navigation to use subnavigation items (#45)
    * changed navigation to use subnavigation items
    
    * Adjusted collapsing logic of navigation component, now with clicks and arrow icon
    
    * Renamed classes and collapsable content
    
    * Moved click event to tab instead of icon
    
    * Changed hover effects in navbar to use primary color
    
    Co-authored-by: Stefan Basaric <stefan.basaric@hotmail.de>

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -10,8 +10,6 @@ import { Router } from '@angular/router';
   styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent implements OnInit {
-  activeLinkIndex = -1;
-  navLinks: any[] = [];
   title = 'qc-atlas-ui';
 
   isHandset$: Observable<boolean> = this.breakpointObserver
@@ -24,48 +22,9 @@ export class NavigationComponent implements OnInit {
   constructor(
     private breakpointObserver: BreakpointObserver,
     private router: Router
-  ) {
-    this.navLinks = [
-      {
-        label: 'Algorithms',
-        link: './algorithms',
-        index: 0,
-      },
-      {
-        label: 'Execution environments',
-        link: './execution-environments/search',
-        index: 1,
-      },
-      {
-        label: '-> Software Platforms',
-        link: './execution-environments/software-platforms',
-        index: 2,
-      },
-      {
-        label: '-> Cloud services',
-        link: './execution-environments/cloud-services',
-        index: 3,
-      },
-      {
-        label: '-> Compute resources',
-        link: './execution-environments/compute-resources',
-        index: 4,
-      },
-      {
-        label: 'Publications',
-        link: './publications',
-        index: 5,
-      },
-    ];
-  }
+  ) {}
 
-  ngOnInit(): void {
-    this.router.events.subscribe((res) => {
-      this.activeLinkIndex = this.navLinks.indexOf(
-        this.navLinks.find((tab) => tab.link === '.' + this.router.url)
-      );
-    });
-  }
+  ngOnInit(): void {}
 
   goToHome(): void {
     this.router.navigate(['/']);
