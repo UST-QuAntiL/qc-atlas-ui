740891f1a262600efc1e9994b13d3e2bbfdb3df8
Valentin Seifermann <valesayfa@t-online.de>
2020-08-16 21:44:00 +0200

    removed duplicated confirm dialog for deletion

--- src/app/components/compute-resource-property/compute-resource-property-list/compute-resource-property-list.component.ts
+++ src/app/components/compute-resource-property/compute-resource-property-list/compute-resource-property-list.component.ts
@@ -47,22 +47,7 @@ export class ComputeResourcePropertyListComponent implements OnInit {
   }
 
   onDelete(element: EntityModelComputeResourcePropertyDto): void {
-    const dialogData: ConfirmDialogData = {
-      title: 'Confirm Delete',
-      message: 'Do you really want to delete this compute resource property?',
-      yesButtonText: 'Yes',
-      noButtonText: 'No',
-    };
-    const dialogRef = this.utilService.createDialog(
-      ConfirmDialogComponent,
-      dialogData
-    );
-
-    dialogRef.afterClosed().subscribe((dialogResult) => {
-      if (dialogResult != null) {
-        this.deleteProperty.emit(element);
-      }
-    });
+    this.deleteProperty.emit(element);
   }
 
   onEdit(element: EntityModelComputeResourcePropertyDto): void {
