0c19bef0db7cbb09486e6b7aeb284a54974791c0
TGrosskopf <38653495+TGrosskopf@users.noreply.github.com>
2020-10-08 14:34:45 +0200

    use util service for latex rendering onInit and after latex dialog closed

--- src/app/components/generics/property-input/text-input.component.ts
+++ src/app/components/generics/property-input/text-input.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { RenderLatexControllerService } from 'api-latex/services/render-latex-controller.service';
 import { LatexEditorDialogComponent } from '../dialogs/latex-editor-dialog.component';
 import { UtilService } from '../../../util/util.service';
 import { DoProvider } from './abstract-value-accessor';
@@ -24,7 +25,10 @@ export class TextInputComponent implements OnInit {
   latexValue: string;
   urlToRenderedPdfBlob: string;
 
-  constructor(private utilService: UtilService) {}
+  constructor(
+    private utilService: UtilService,
+    private latexRendererService: RenderLatexControllerService
+  ) {}
 
   saveChanges(): void {
     if (this.opensLatexEditor) {
@@ -49,67 +53,48 @@ export class TextInputComponent implements OnInit {
     window.open(this.inputValue, '_blank');
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
     if (this.opensLatexEditor) {
       this.latexValue = this.value;
       if (this.value) {
-        this.updateUrlToRenderedPdfBlob(this.latexValue);
+        this.renderLatexContent(this.latexValue);
       }
     } else {
       this.inputValue = this.value;
     }
   }
 
-  dataURItoBlob(dataURI): Blob {
-    // convert base64 to raw binary data held in a string
-    // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
-    const byteString = atob(dataURI.split(',')[1]);
-
-    // separate out the mime component
-    const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-
-    // write the bytes of the string to an ArrayBuffer
-    const ab = new ArrayBuffer(byteString.length);
-    const ia = new Uint8Array(ab);
-    for (let i = 0; i < byteString.length; i++) {
-      ia[i] = byteString.charCodeAt(i);
-    }
-
-    // write the ArrayBuffer to a blob
-    return new Blob([ab], { type: mimeString });
-  }
-
-  updateUrlToRenderedPdfBlob(stringValue: string): any {
-    this.urlToRenderedPdfBlob = URL.createObjectURL(
-      this.dataURItoBlob(stringValue)
-    );
-  }
-
-  openLatexEditor() {
+  openLatexEditor(): void {
     const doalogRef = this.utilService.createDialog(
       LatexEditorDialogComponent,
       {
         title: 'LaTeX Render Editor',
-        inputText: 'Example equation: $\\sqrt{16}=2^{2}$',
-        latexPackages: ['\\usepackage{amsmath}'],
-        // eslint-disable-next-line max-len
-        //   'content":"\\begin{quantikz}\n\t\\lstick{$\\ket{0}$} & \\gate{H} & \\ctrl{1} & \\gate{H} & \\ctrl{1} & \\swap{2} & \\ctrl{1} & \\qw \\\\ \n \t \\lstick{$\\ket{0}$} & \\gate{D} & \\targ{} & \\octrl{-1} & \\control{} & \\qw & \\octrl{1} & \\qw \\\\ \n \t &&&&& \\targX{} & \\gate{F} & \\qw \n \\end{quantikz}',
-        // latexPackages: ['\\usepackage{tikz}', '\\usetikzlibrary{quantikz}'],
+        inputText:
+          //   'Example equation: $\\sqrt{16}=2^{2}$',
+          // latexPackages: ['\\usepackage{amsmath}'],
+          // eslint-disable-next-line max-len
+          'content":"\\begin{quantikz}\n\t\\lstick{$\\ket{0}$} & \\gate{H} & \\ctrl{1} & \\gate{H} & \\ctrl{1} & \\swap{2} & \\ctrl{1} & \\qw \\\\ \n \t \\lstick{$\\ket{0}$} & \\gate{D} & \\targ{} & \\octrl{-1} & \\control{} & \\qw & \\octrl{1} & \\qw \\\\ \n \t &&&&& \\targX{} & \\gate{F} & \\qw \n \\end{quantikz}',
+        latexPackages: ['\\usepackage{tikz}', '\\usetikzlibrary{quantikz}'],
         output: 'pdf',
-      }
+      },
+      undefined
     );
 
     doalogRef.afterClosed().subscribe((dialogResult) => {
-      const latexBlob = new Blob([dialogResult], { type: 'application/pdf' });
-      const fileReader = new FileReader();
-      fileReader.readAsDataURL(latexBlob);
-      fileReader.onload = () => {
-        this.latexValue = fileReader.result.toString();
-        this.updateUrlToRenderedPdfBlob(this.latexValue);
-        if (this.latexValue !== this.value) {
-          this.inputChanged();
-        }
-      };
+      console.log(dialogResult);
+      if (dialogResult !== this.latexValue) {
+        this.latexValue = dialogResult;
+        this.renderLatexContent(this.latexValue);
+        this.inputChanged();
+      }
     });
+  }
+
+  private renderLatexContent(latexValue: string): any {
+    return this.utilService
+      .renderLatexContentAndReturnUrlToPdfBlob(latexValue)
+      .subscribe((blobUrl) => {
+        this.urlToRenderedPdfBlob = blobUrl;
+      });
   }
 }
