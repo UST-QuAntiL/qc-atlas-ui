037187e5252dca9e32dfbaea5cc0402016251c3e
salmma <github@mielie.de>
2021-03-10 16:39:04 +0100

    switch button when execute clicked

--- src/app/components/algorithms/nisq-analyzer/nisq-analyzer.component.ts
+++ src/app/components/algorithms/nisq-analyzer/nisq-analyzer.component.ts
@@ -228,6 +228,7 @@ export class NisqAnalyzerComponent implements OnInit {
           )
           .subscribe((finalResult) => (this.results = finalResult));
       }
+      this.hasExecutionResult(analysisResult);
     });
   }
 
