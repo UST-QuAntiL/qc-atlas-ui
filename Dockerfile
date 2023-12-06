14f3af8e83994a5f31d765e753e7321ebbfb4643
Daniel Georg <daniel.georg@iaas.uni-stuttgart.de>
2023-12-06 18:42:16 +0100

    versioning, small fixes, lint-fix

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ WORKDIR /app
 
 COPY . .
 
-RUN npm ci && npm run build --prod
+RUN npm install && npm run build --prod
 
 FROM nginx:alpine
 COPY --from=builder app/dist/* /usr/share/nginx/html
--- src/app/components/qunicorn-app/histogram-plot/histogram-plot.component.ts
+++ src/app/components/qunicorn-app/histogram-plot/histogram-plot.component.ts
@@ -1,69 +1,66 @@
 import { Component, OnChanges, OnInit, ViewChild, Input } from '@angular/core';
 import { Chart } from 'chart.js';
 
-
-
 @Component({
   selector: 'app-histogram-plot',
   templateUrl: './histogram-plot.component.html',
-  styleUrls: ['./histogram-plot.component.scss']
+  styleUrls: ['./histogram-plot.component.scss'],
 })
 export class HistogramPlotComponent implements OnChanges {
-
   @ViewChild('plot', { static: true }) plotDiv;
 
   @Input() counts: { [props: string]: number } | null = null;
 
-  constructor() { }
+  constructor() {}
 
   ngOnChanges(): void {
-
     const labels = Object.keys(this.counts);
 
     const countsData = [];
-    labels.forEach(label => countsData.push(this.counts[label]));
+    labels.forEach((label) => countsData.push(this.counts[label]));
 
     console.log(labels, countsData);
 
     const data = {
-      labels: labels,//Object.keys(this.counts),
+      labels, // Object.keys(this.counts),
       datasets: [
         {
           label: 'Counts',
-          data: countsData,// Object.values(this.counts),
-          backgroundColor: "#3f51b5"
+          data: countsData, // Object.values(this.counts),
+          backgroundColor: '#3f51b5',
         },
-      ]
+      ],
     };
 
     const config = {
       type: 'bar',
-      data: data,
+      data,
       options: {
         scales: {
-          yAxes: [{
+          yAxes: [
+            {
               ticks: {
-              beginAtZero: true,
-              min: 0,
-            }
-          }]
-        }
+                beginAtZero: true,
+                min: 0,
+              },
+            },
+          ],
+        },
       },
-    }
+    };
 
     const div: HTMLDivElement = this.plotDiv.nativeElement;
-    div.innerHTML = "";
-    const canvas = document.createElement("canvas");
-    canvas.setAttribute("height", "50px");
-    canvas.setAttribute("width", "50px");
+    div.innerHTML = '';
+    const canvas = document.createElement('canvas');
+    canvas.setAttribute('height', '50px');
+    canvas.setAttribute('width', '50px');
     canvas.width = 500;
     canvas.height = 300;
     const context = canvas.getContext('2d');
     context.scale(0.5, 0.5);
-    //chartcontext.scale(50, 50);
+    // chartcontext.scale(50, 50);
     div.appendChild(canvas);
 
     new Chart(context, config);
   }
-
 }
